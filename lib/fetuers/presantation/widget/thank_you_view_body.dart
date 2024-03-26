ca004cca5ac4bd9d4a29d11b6bc2d692ae120bd9
a7iiid <naz131681@gmail.com>
2024-03-26 22:20:11 +0200

    containar shape

--- lib/fetuers/presantation/widget/thank_you_view_body.dart
+++ lib/fetuers/presantation/widget/thank_you_view_body.dart
@@ -5,6 +5,33 @@ class ThankYouViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Stack(
+        children: [
+          Container(
+            decoration: ShapeDecoration(
+              color: Color(0xFFD9D9D9),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+          Positioned(
+            left: -20,
+            bottom: MediaQuery.sizeOf(context).height * .2,
+            child: const CircleAvatar(
+              backgroundColor: Colors.white,
+            ),
+          ),
+          Positioned(
+              right: -20,
+              bottom: MediaQuery.sizeOf(context).height * .2,
+              child: const CircleAvatar(
+                backgroundColor: Colors.white,
+              ))
+        ],
+      ),
+    );
   }
 }
