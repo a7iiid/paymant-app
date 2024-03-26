9a8ac87b64c70ed86ee5f9a73d83cf0069380bfb
a7iiid <naz131681@gmail.com>
2024-03-27 01:03:53 +0200

    text style

---
+++ lib/core/utlis/appStyle.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class AppStyle {
+  static TextStyle normal25 = const TextStyle(
+    color: Colors.black,
+    fontSize: 25,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+  static TextStyle regylar20 = TextStyle(
+    color: Colors.black.withOpacity(0.800000011920929),
+    fontSize: 20,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
+  static TextStyle regylar18 = const TextStyle(
+    color: Colors.black,
+    fontSize: 18,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
+
+  static TextStyle bolde18 = const TextStyle(
+    color: Colors.black,
+    fontSize: 18,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+  static TextStyle bold24 = const TextStyle(
+    color: Colors.black,
+    fontSize: 24,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+}
